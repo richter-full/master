22127d2f05cf8670b6f9efb0959291d88cbdb06d
Tobias Gebert <tobias.gebert@diva-e.com>
2019-05-18 18:21:35 +0200

    Content

---
+++ www/site/accounts/admin.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+password: >
+  $2y$10$KsPGPZlmROQwFpuLFvKVq.cXzKndK5R4C3uAvodwujzm5K3UWNcvO
+email: horstschacht@ok.de
+language: de
+role: admin
+history:
+  - articles/icsfed1541
+  - articles/achfcx2021
+  - articles/sdbbfde1534
+  - articles/sdvfaa1443
+  - articles/sdvfxd1553
